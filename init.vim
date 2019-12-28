15f4f932e8f71c1078de6edb80edd44da63f293d
Виталий Старов <v.starov@superjob.ru>
2019-12-28 15:47:26 +0300

    Minor fixes

--- init.vim
+++ init.vim
@@ -1,3 +1,9 @@
+" Install vim-plug if not present
+if empty(glob('~/.config/nvim/autoload/plug.vim'))
+  silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall
+endif
+
 " Plugins
 call plug#begin('~/.vim/plugged')
 
--- install.sh
+++ install.sh
@@ -11,14 +11,16 @@ apt upgrade
 # tig
 apt install tig
 
-# oh-my-zsh & tools
+# zsh & tools
+apt install zsh
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/denysdovhan/spaceship-prompt.git \
-  "$ZSH_CUSTOM/themes/spaceship-prompt"
+  "${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/spaceship-prompt"
 
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" \
-  "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -s "${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/spaceship-prompt/spaceship.zsh-theme" \
+  "${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/spaceship.zsh-theme"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions \
   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -26,10 +28,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions \
 # nvim & tools
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
+mkdir ../Applications
+mv nvim.appimage ../Applications/
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim
 
 pip3 install --user pynvim
 
@@ -42,10 +44,10 @@ apt install golang-go
 
 # -- links ------------------------------------------------------------------------------
 
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sfn ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
+ln -sfn ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sfn ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 
 
 # -- directories ------------------------------------------------------------------------
